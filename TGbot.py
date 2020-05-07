eb077bd6531c02b862bf99fc5116374e642e090e
zkeka21 <63802033+zkeka21@users.noreply.github.com>
2020-05-07 21:33:25 +0400

    Update TGbot.py

--- TGbot.py
+++ TGbot.py
@@ -4,21 +4,7 @@ bot = telebot.TeleBot('1250764561:AAH04Jx6rh5OFYcLofdLN9ne52QRdm6RafA')
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if message.text.lower() == 'ок':
-        bot.send_message(message.chat.id, 'ок')
-    elif message.text.lower() == 'Ок':
-        bot.send_message(message.chat.id, 'ОК')
-    elif message.text.lower() == 'ok':
-        bot.send_message(message.chat.id, 'ok')
-    elif message.text.lower() == 'oк':
-        bot.send_message(message.chat.id, 'ok')
-    elif message.text.lower() == 'оk':
-        bot.send_message(message.chat.id, 'ok'  )
-    elif message.text.lower() == '0k':
-        bot.send_message(message.chat.id, 'ok')
-    elif message.text.lower() == '0к':
-        bot.send_message(message.chat.id, 'ok')
-    elif message.text.lower() == 'Вань':
+    if message.text.lower() == 'Вань':
         bot.send_message(message.chat.id, 'Воробьев')
     elif message.text.lower() == 'Ваня':
         bot.send_message(message.chat.id, 'Воробьев')
